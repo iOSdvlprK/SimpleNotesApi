eec9fa056e6d2349593720579ef2f1f4785b1792
iOS developer K <iOSdvlprK@gmail.com>
2023-07-06 19:02:34 +0900

    handle CRUD REST API routes

--- src/index.js
+++ src/index.js
@@ -9,26 +9,64 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/notes", (req, res) => {
-  fs.readFile(__dirname + '/' + "notes.json", 'utf-8', (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
+// CRUD
 
-    res.status(200).send(data);
-  })
-});
-
-app.post('/notes', (req, res) => {
+// CREATE
+app.post('/notes', async (req, res) => {
   const note = new Note(req.body);
 
-  note.save()
-  .then(() => {
-    res.status(200).send(note);
-  })
-  .catch((err) => {
+  try {
+    await note.save();
+    res.status(201).send(note);
+  }
+  catch (err) {
+    res.status(400).send(err);
+  }
+})
+
+// READ
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.send(notes);
+  }
+  catch (err) {
     res.status(500).send(err);
-  })
+  }
+});
+
+// UPDATE
+app.patch('/notes/:id', async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send();
+    }
+
+    note.note = req.body.note;
+    await note.save();
+    res.status(200).send(note);
+  }
+  catch (err) {
+    res.status(404).send(err);
+  }
+})
+
+// DELETE
+app.delete('/notes/:id', async (req, res) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+
+    if (!note) {
+      return res.status(404).send();
+    }
+    
+    res.send("The note has been deleted");
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
 })
 
 app.listen(3000, () => {
